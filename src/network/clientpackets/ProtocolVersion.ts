b81dd70fd5b5a4e29e3fde5cb3711d5662f0595b
npetrovski <to.petrovski@gmail.com>
2020-11-14 16:43:29 +0200

    ProtocolVersion fix

--- src/network/clientpackets/ProtocolVersion.ts
+++ src/network/clientpackets/ProtocolVersion.ts
@@ -4,7 +4,7 @@ export default class ProtocolVersion extends GameServerPacket {
   private _protocolVersion = 656; // use value=-2 in order to "ping"
 
   write(): void {
-    this.writeC(0x0e);
+    this.writeC(0x00);
     this.writeD(this._protocolVersion);
   }
 }
