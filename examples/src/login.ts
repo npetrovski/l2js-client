fccbc03f49b1f302d826aeec4823ed847ec46462
W3nder <rtz645@gmail.com>
2020-11-20 19:56:51 -0300

    change example login

--- examples/src/login.ts
+++ examples/src/login.ts
@@ -2,9 +2,9 @@ import Client from "l2js-client/Client";
 
 const l2 = new Client();
 l2.enter({
-  /* required */ Username: "unkbows",
-  /* required */ Password: "senha01#",
-  /* required */ Ip: "104.238.223.242",
+  /* required */ Username: "admin",
+  /* required */ Password: "admin",
+  /* required */ Ip: "127.0.0.1",
   /* optional */ ServerId: 1, //Bartz 
   /* optional */ CharSlotIndex: 0,
 });
