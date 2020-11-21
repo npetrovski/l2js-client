ffba4711ec4494900cfaedbffb408f1b58b0b9e4
W3nder <rtz645@gmail.com>
2020-11-20 22:16:42 -0300

    remove examples

--- examples/src/follow.ts
+++
@@ -1,9 +0,0 @@
-import l2 from "./login";
-import L2Creature from "l2js-client/entities/L2Creature";
-import { EStartMoving } from "l2js-client/events/EventTypes";
-
-l2.on("StartMoving", (e: EStartMoving) => {
-  if (e.data.creature.Name === "Adm") {
-    l2.moveTo(e.data.creature.Dx, e.data.creature.Dy, e.data.creature.Dz);
-  }
-});
--- examples/src/respond-to-attack.ts
+++
@@ -1,9 +0,0 @@
-import l2 from "./login";
-import { EAttacked } from "l2js-client/events/EventTypes";
-
-l2.on("Attacked", (e: EAttacked) => {
-  if (Array.from(e.data.subjects).indexOf(l2.Me.ObjectId) !== -1) {
-    l2.hit(e.data.object);
-    l2.hit(e.data.object);
-  }
-});
