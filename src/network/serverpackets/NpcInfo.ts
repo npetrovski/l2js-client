4ba41e290274c09ffdf26886aed514af3478968b
W3nder <rtz645@gmail.com>
2020-11-20 22:27:31 -0300

    changing some package.

--- src/network/serverpackets/NpcInfo.ts
+++ src/network/serverpackets/NpcInfo.ts
@@ -11,6 +11,7 @@ export default class NpcInfo extends AbstractNpcInfo {
     const _isAttackable = this.readD() === 1;
 
     let npc = this.Client.CreaturesList.getEntryByObjectId(_objId);
+
     if (!npc) {
       if (!_isAttackable) {
         npc = new L2Npc();
@@ -30,6 +31,7 @@ export default class NpcInfo extends AbstractNpcInfo {
     npc.Heading = this.readD();
 
     const _pad1 = this.readD();
+
     const MAtkSpd = this.readD();
     const PAtkSpd = this.readD();
 
@@ -65,35 +67,6 @@ export default class NpcInfo extends AbstractNpcInfo {
     npc.Name = this.readS();
     const _unkn3 = this.readD();
     npc.Title = this.readS();
-
-    const _pad2 = this.readD();
-    const _pad3 = this.readD();
-    const _pad4 = this.readD();
-
-    // let _titleColor = this.readD(); // Title color 0=client default
-    // let _pvpFlag = this.readD(); // pvp flag
-    // let _karma = this.readD(); // karma
-
-    const _invisibleVisualEffect = this.readD();
-    const _clanId = this.readD();
-    const _clanCrest = this.readD();
-    const _allyId = this.readD();
-    const _allyCrest = this.readD();
-
-    const _insideZone = this.readC(); // 1=water, 2=flying
-    const _teamId = this.readC();
-
-    const _collisionRadius1 = this.readF();
-    const _collisionHeight2 = this.readF();
-    const _enchantEffect = this.readD(); // C4
-    const _isFlying = this.readD() === 1; // C6
-    const _pad5 = this.readD();
-    const _colorEffect = this.readD(); // CT1.5 Pet form and skills, Color effect
-
-    npc.IsTargetable = this.readC() === 1;
-    const _isShowName = this.readC() === 1;
-    const _abnormalVisualEffectSpecial = this.readD();
-    const _displayEffect = this.readD();
 
     npc.calculateDistance(this.Client.ActiveChar);
 
