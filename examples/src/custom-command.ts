e61787b0f3106ed808d166521a06a1c7b6b189b2
Nikolay Petrovski <to.petrovski@gmail.com>
2021-10-16 15:41:13 +0300

    Update custom-command example

--- examples/src/custom-command.ts
+++ examples/src/custom-command.ts
@@ -1,13 +1,12 @@
 import l2 from "./login";
 
 import AbstractGameCommand from "l2js-client/commands/AbstractGameCommand";
-import GameClient from "l2js-client/network/GameClient";
 
 l2.registerCommand("sayHello", {
   execute(text: string): void {
-    console.log(this.Client.ActiveChar.Name + ": " + text);
+    console.log(this.GameClient.ActiveChar.Name + ": " + text);
   }
-} as AbstractGameCommand<GameClient>);
+} as AbstractGameCommand);
 
 l2.on("LoggedIn", () => {
   (l2 as any).sayHello("Hello");
